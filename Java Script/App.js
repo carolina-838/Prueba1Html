80cbb0406a4a9ade3bdd76bef3a3cc84e48f3ac1
Diana Arias <da647942@gmail.com>
2024-09-18 08:11:21 -0500

    -

--- Java Script/App.js
+++ Java Script/App.js
@@ -145,14 +145,147 @@ default: console.log("Operador n existe");
 // Incremento,
 
 
-let tabla = prompt("Agrega un número para multiplicar");
+// let tabla = prompt("Agrega un número para multiplicar");
 
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${tabla} * ${i} = ${tabla * i}`);
-    }
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${tabla} * ${i} = ${tabla * i}`);
+//     }
 
 // // Decremento
 // for(let j = 20; j>=1; j-2){
 //     console.log(j)
 // }
 
+// Mas ejercicios de FOR
+
+// 1
+
+// let n = parseInt(prompt("introduce un numero positivo"))
+// let sum =0
+
+// for(let i=1; i <=n; i++) {
+
+//     sum +=n
+
+// }    
+
+// console.log(`La suma de los numeros del 1 al ${n} es: ${sum} `)
+
+// 2
+
+// let tabla = parseInt(prompt("Agrega un número para multiplicar"));
+
+//     for (let i = 1; i <= 10; i++) {
+//     for(let j = 1; j<= 10; j++ ){
+//     console.log(`${i} * ${j} = ${i * j}`)
+    
+// }
+// console.log(" ")
+// }
+
+// 3
+
+// let sum = 0;   
+
+// for (let i = 1; i <= 199; i += 2) {  
+//     sum += i; 
+// }  
+
+// console.log(`La suma de los números impares del 1 al 99 es: ${sum}`); 
+
+// // let n=1;
+// // let sum = 0
+
+// // while(n>0){
+// // n = parseInt(prompt("Ingrese un numeros positivos para sumarlos y finalice para obtener el resultado digitando un numero negativo"))
+
+// // if(n>0){
+// // sum+=n}
+// // }
+
+// console.log(`lo que suman los numeros ingresados es:${(sum)}`)
+
+// Desarollar menu de Opciones
+
+
+let Estado= true;
+let coseno, seno, raiz, numero;
+
+// opciones realizar un calculo matematico
+// 1. realizar
+// 2. Seno
+// 3. Coseno
+
+// while(Estado){
+
+//     menu = (parseInt(prompt(`SELECCIONE UN OPCION \n 1. Raiz \n 2. Seno \n 3. Coseno \n 4. Salir`)));
+    
+//     switch(menu){
+    
+//       case 1: alert("Selecciono Raiz");
+    
+//       numero = (parseInt(prompt(`INGRESE EL NUMERO PARA CALCULAR LA RAIZ`)));
+       
+//       raiz = Math.sqrt(numero);
+    
+//       alert(`LA RAIZ DE ${numero} ES: ${raiz}`);
+    
+//       break;
+//     case 2 : alert("Selecciono Seno N1");
+    
+//       numero = (prompt(`INGRESE EL NUMERO PARA CALCULAR EL SENO`));
+       
+//       seno = Math.sin(numero);
+    
+//       alert(`EL SENO DE ${numero} ES: ${seno}`);
+    
+//       break;
+    
+//       case 3 : alert("Selecciono Coseno N1");
+    
+//       numero = (prompt(`INGRESE EL NUMERO PARA CALCULAR EL COSENO`));
+       
+//       coseno = Math.sin(numero);
+    
+//       alert(`EL COSENO DE ${numero} ES: ${coseno}`);
+    
+//       break;
+    
+//       case 4: alert("SALIENDO....")
+//       estado = falso;
+//       break;
+//     default:
+//         alert("LA OPCION NO EXISTE");
+    
+ 
+//     }
+//     }
+
+
+// //   let numeroAleatorio = Math.floor(Math.random() * 50) + 1;
+
+// //  while (true) {
+// //   let N = prompt("Adivina el número entre 1 y 50, Buena suerte:");
+  
+// //   if (isNaN(N)) {
+// //     alert("Ingrese un número válido")
+// //   }
+
+// //   if (N < 1 ||  N > 50) {
+// //     alert("Ingrese un número entre 1 y 50")
+// //   }
+  
+// //   if (N = numeroAleatorio) {
+// //     alert("Adivinaste!");
+// //     break;
+
+// //   } 
+  
+// //   if (N < numeroAleatorio) {
+// //     alert("No, el número es mayor");
+// //   } 
+  
+// //   else {
+// //     alert("No, el número es menor");
+// //   }
+// // }
---
+++ Java Script/App2.js
@@ -0,0 +1,31 @@
+// Ejercicio switch Case
+
+let menu, bebida; 
+menu = prompt("Seleccione el menú que desea el día de hoy: Carne, Pescado, Ensalada ").toLowerCase());
+
+switch (menu){
+
+ case "Carne":
+ bebida = "¿deseas tomar vino?"
+ break;
+
+ case "Pescado": 
+ bebida = "¿deseas tomar vino blanco?"
+ break;
+
+ case "Ensalada": 
+ bebida = "¿deseas tomar Agua?"
+ break;
+
+ default:
+ bebida = ("Elija Carne, pescado o ensalada.");
+ break;
+}
+
+ alert=bebida
+
+ 
+//  Games
+
+
+
