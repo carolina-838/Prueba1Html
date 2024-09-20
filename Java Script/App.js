2ff707ee5a8c0131f4722616b09b52466092f4c8
Diana Arias <da647942@gmail.com>
2024-09-19 22:36:21 -0500

    -

--- Java Script/App.js
+++ Java Script/App.js
@@ -218,74 +218,253 @@ let coseno, seno, raiz, numero;
 
 // while(Estado){
 
-//     menu = (parseInt(prompt(`SELECCIONE UN OPCION \n 1. Raiz \n 2. Seno \n 3. Coseno \n 4. Salir`)));
+// //     menu = (parseInt(prompt(`SELECCIONE UN OPCION \n 1. Raiz \n 2. Seno \n 3. Coseno \n 4. Salir`)));
     
-//     switch(menu){
+// //     switch(menu){
     
-//       case 1: alert("Selecciono Raiz");
+// //       case 1: alert("Selecciono Raiz");
     
-//       numero = (parseInt(prompt(`INGRESE EL NUMERO PARA CALCULAR LA RAIZ`)));
+// //       numero = (parseInt(prompt(`INGRESE EL NUMERO PARA CALCULAR LA RAIZ`)));
        
-//       raiz = Math.sqrt(numero);
+// //       raiz = Math.sqrt(numero);
     
-//       alert(`LA RAIZ DE ${numero} ES: ${raiz}`);
+// //       alert(`LA RAIZ DE ${numero} ES: ${raiz}`);
     
-//       break;
-//     case 2 : alert("Selecciono Seno N1");
+// //       break;
+// //     case 2 : alert("Selecciono Seno N1");
     
-//       numero = (prompt(`INGRESE EL NUMERO PARA CALCULAR EL SENO`));
+// //       numero = (prompt(`INGRESE EL NUMERO PARA CALCULAR EL SENO`));
        
-//       seno = Math.sin(numero);
+// //       seno = Math.sin(numero);
     
-//       alert(`EL SENO DE ${numero} ES: ${seno}`);
+// //       alert(`EL SENO DE ${numero} ES: ${seno}`);
     
-//       break;
+// //       break;
     
-//       case 3 : alert("Selecciono Coseno N1");
+// //       case 3 : alert("Selecciono Coseno N1");
     
-//       numero = (prompt(`INGRESE EL NUMERO PARA CALCULAR EL COSENO`));
+// //       numero = (prompt(`INGRESE EL NUMERO PARA CALCULAR EL COSENO`));
        
-//       coseno = Math.sin(numero);
+// //       coseno = Math.sin(numero);
     
-//       alert(`EL COSENO DE ${numero} ES: ${coseno}`);
+// //       alert(`EL COSENO DE ${numero} ES: ${coseno}`);
     
-//       break;
+// //       break;
     
-//       case 4: alert("SALIENDO....")
-//       estado = falso;
-//       break;
-//     default:
-//         alert("LA OPCION NO EXISTE");
+// //       case 4: alert("SALIENDO....")
+// //       estado = falso;
+// //       break;
+// //     default:
+// //         alert("LA OPCION NO EXISTE");
     
  
-//     }
-//     }
+// //     }
+// //     }
 
 
-// //   let numeroAleatorio = Math.floor(Math.random() * 50) + 1;
+// // //   let numeroAleatorio = Math.floor(Math.random() * 50) + 1;
 
-// //  while (true) {
-// //   let N = prompt("Adivina el número entre 1 y 50, Buena suerte:");
+// // //  while (true) {
+// // //   let N = prompt("Adivina el número entre 1 y 50, Buena suerte:");
   
-// //   if (isNaN(N)) {
-// //     alert("Ingrese un número válido")
-// //   }
+// // //   if (isNaN(N)) {
+// // //     alert("Ingrese un número válido")
+// // //   }
 
-// //   if (N < 1 ||  N > 50) {
-// //     alert("Ingrese un número entre 1 y 50")
-// //   }
+// // //   if (N < 1 ||  N > 50) {
+// // //     alert("Ingrese un número entre 1 y 50")
+// // //   }
   
-// //   if (N = numeroAleatorio) {
-// //     alert("Adivinaste!");
-// //     break;
+// // //   if (N = numeroAleatorio) {
+// // //     alert("Adivinaste!");
+// // //     break;
 
-// //   } 
+// // //   } 
   
-// //   if (N < numeroAleatorio) {
-// //     alert("No, el número es mayor");
-// //   } 
+// // //   if (N < numeroAleatorio) {
+// // //     alert("No, el número es mayor");
+// // //   } 
   
-// //   else {
-// //     alert("No, el número es menor");
-// //   }
-// // }
+// // //   else {
+// // //     alert("No, el número es menor");
+// // //   }
+// // // }
+
+// let ac, acu, at;
+
+// let r, lado, b , h;
+
+// const (pi) = 3.1416;
+
+// let opcion;
+
+// let (estado) = true;
+
+// do{
+
+//   opcion = parseInt(prompt(`1. Area Circulo \n 2. Area del Cuadrado \n 3. Area del Triangulo \n 4. Salir`));
+  
+
+//   switch(opcion){
+
+
+//     case 1: alert("Area del circulo");
+//     // A = pi * r*r
+//     r = parseFloat(prompt("Ingrese el valor del radio"));
+
+//     ac = pi * (r*r);
+
+//     alert(`El area del circulo es: ${ac}`);
+
+//     break;
+// case 2: alert("Area del cuadrado");
+
+//    // A = lado * lado
+
+//    lado = parseFloat(prompt("Ingrese el valor del lado"));
+
+//    acu = (lado*lado);
+
+//    alert(`El area del cuadrado es: ${acu}`);
+
+//     break;
+
+//     case 3: alert("Area del triangulo");
+
+//     // A = B*H/2
+
+//     b = parseFloat(prompt("Ingrese el valor de la base"));
+//     h = parseFloat(prompt("Ingrese el valor de la altura"));
+
+//     at = (b*h)/2;
+
+//     alert(`El area del triangulo es: ${at}`);
+
+//     break;
+// case 4:
+//       alert("SALIENDO.....");
+//       estado = false;
+
+//     break;
+
+//     default: 
+//     alert("Opcion no valida");
+
+  
+
+//   }
+
+
+// }while(estado);
+
+
+let KC, a
+
+let KF, b
+
+let FC, c
+
+let FK, d
+
+let CK, e
+
+let CF, f
+
+
+let status = true;
+let opcion;
+
+
+do{
+
+  opcion = parseInt(prompt(` 1. Kelvin a Celsius \n 2. Kelvin a Fahrenheit \n 3. Fahrenheit a Celsius \n 4. Fahrenheit a Kelvin \n 5. Celsius a Kelvin \n 6. Celsius a Fahrenheit`));
+  
+
+  switch(opcion){
+
+
+    case 1: alert("Kelvin a Celsius");
+    // C=K-273.15
+    a = parseFloat(prompt("Ingrese el valor en Kelvin"));
+
+    KC= a-273.15
+
+    alert(`La conversion es igual a: ${KC} Grados Celsius`);
+
+    break;
+
+case 2: alert("Kelvin a Fahrenheit");
+
+   // F= (9*(K-273.15)/5)+32
+
+   b = parseFloat(prompt("Ingrese el valor en Kelvin"));
+
+   KF = (9*(b-273.15)/5)+32;
+
+   alert(`La conversion es igual a: ${KF} Grados Fahrenheit`);
+
+    break;
+
+    case 3: alert("Fahrenheit a celsius");
+
+    // C= (5*(F-32))/9
+
+    c = parseFloat(prompt("Ingrese el valor en Fahrenheit"));
+    
+    FC = (5*(C-32))/9
+
+    alert(`La conversion es igual a: ${FC} Grados Fahrenheit`);
+
+    break;
+
+    case 4: alert("Fahrenheit a Kelvin");
+
+    // K= ((5*(F-32))/9)+273.15
+
+    d = parseFloat(prompt("Ingrese el valor en Fahrenheit"));
+    
+    FK = ((5*(d-32))/9)+273.15
+
+    alert(`La conversion es igual a: ${FK} Grados Kelvin`);
+
+    break;
+
+    case 5: alert("Celsius a Kelvin");
+
+    // K= C+273.15
+
+    e = parseFloat(prompt("Ingrese el valor en Celsius"));
+    
+    CK = e+273.15
+
+    alert(`La conversion es igual a: ${CK} Grados Kelvin`);
+
+    break;
+
+    case 6: alert("Celsius a Fahrenheit");
+
+    // F= (9*C)/5)+32
+
+    f = parseFloat(prompt("Ingrese el valor en Celsius"));
+    
+    CF = ((9*f)/5)+32
+
+    alert(`La conversion es igual a: ${CF} Grados Fahrenheit`);
+
+    break;
+    
+case 7:
+      alert("SALIENDO.....");
+      estado = false;
+
+    break;
+
+    default: 
+    alert("Opcion no valida");
+
+  
+
+  }
+
+
+}while(estado);
